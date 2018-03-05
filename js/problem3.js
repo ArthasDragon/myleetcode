aece731a83126750946490c375690c89b3f6ec4e
an_ying <ywl123698745>
2018-03-05 19:35:48 +0800

    leetcode

--- js/problem3.js
+++ js/problem3.js
@@ -1,11 +1,16 @@
-import React, {Component} from 'react'
-import {observer} from 'mobx-react'
-
-@observer
-export default class extends Component {
-  render() {
-    return (
-      < div > login < /div>
-  )
-  }
-}
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  var arr = s.split('')
+  var obj = {}
+  var max_len = arr.length ? 1 : 0
+  var behind = -1
+  arr.map((item,index) => {
+    behind = obj[item]==undefined?behind:(behind>obj[item]?behind:obj[item])
+    obj[item] = index
+    max_len = max_len>=(index-behind)?max_len:(index-behind)
+  })
+  return max_len
+};
--- js/problem4.js
+++ js/problem4.js
@@ -1,11 +1,28 @@
-import React, {Component} from 'react'
-import {observer} from 'mobx-react'
-
-@observer
-export default class extends Component {
-  render() {
-    return (
-      < div > login < /div>
-  )
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+  var total_arr = []
+  var numb = null
+  while (nums1.length && nums2.length){
+    var value1 = nums1[0]
+    var value2 = nums2[0]
+    if(value1<=value2){
+      total_arr.push(value1)
+      nums1.shift()
+    }else {
+      total_arr.push(value2)
+      nums2.shift()
+    }
   }
-}
+  total_arr = total_arr.concat(nums1,nums2)
+  var len = total_arr.length
+  if(len%2==0){
+    numb = (total_arr[len/2 - 1] + total_arr[len/2])/2
+  }else {
+    numb = total_arr[(len-1)/2]
+  }
+  return numb
+};
