853ea610afcb9fc5f0836b6933a22e1eac1198fe
an_ying <ywl123698745>
2018-03-05 15:58:57 +0800

    leetcode

--- js/problem1.js
+++ js/problem1.js
@@ -1,11 +1,20 @@
-import React, {Component} from 'react'
-import {observer} from 'mobx-react'
-
-@observer
-export default class extends Component {
-  render() {
-    return (
-      < div > login < /div>
-  )
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  let obj = {}
+  let value = []
+  nums.map((item,index) => {
+    obj[item] = index + 1
+  })
+  for(let i=0,len=nums.length; i<len; ++i){
+    let pos = obj[target-nums[i]]
+    if(pos && pos!=i+1){
+      value = [pos-1,i]
+      return value
+    }
   }
 }
+console.log(twoSum([2,4,6,8],14))
--- js/problem2.js
+++ js/problem2.js
@@ -1,11 +1,47 @@
-import React, {Component} from 'react'
-import {observer} from 'mobx-react'
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+  let result_list = null
+  let add = false
+  let list1 = l1
+  let list2 = l2
+  let current_list = null
 
-@observer
-export default class extends Component {
-  render() {
-    return (
-      < div > login < /div>
-  )
+  while (list1 || list2){
+    let v1 = list1 && list1.val || 0
+    let v2 = list2 && list2.val || 0
+    let sum = add ? v1+v2+1 : v1+v2
+    if(sum >= 10){
+      add = true
+    }else {
+      add = false
+    }
+    if(sum == 9 && add){
+      add = true
+      sum = 0
+    }
+    if(result_list){
+      current_list.next = new ListNode(sum%10)
+      current_list = current_list.next
+    }else {
+      result_list = new ListNode(sum%10)
+      current_list = result_list
+    }
+    list1 = list1&&list1.next || null
+    list2 = list2&&list2.next || null
   }
+  if(add){
+    current_list.next = new ListNode(1)
+  }
+  return result_list
 }
