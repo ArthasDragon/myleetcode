4c8ebf0c651ecc1a0e557c6135842f13578bc32f
an_ying <ywl123698745>
2018-04-02 17:14:40 +0800

    leetcode

---
+++ js/problem17.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import {observer} from 'mobx-react'
+
+@observer
+export default class extends Component {
+    render() {
+        return (
+            < div > login < /div>
+    )
+    }
+}
