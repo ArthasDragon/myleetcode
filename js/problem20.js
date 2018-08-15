a01f83f74ac70657985292528c027020acd337d5
Dragon <1641627793@qq.com>
2018-08-15 19:00:58 +0800

    problem20

--- js/problem20.js
+++ js/problem20.js
@@ -6,4 +6,16 @@ let isValid = function(s) {
   if (s === undefined || s === null) {
     return false;
   }
+  const len = s.length;
+  if (len % 2 !== 0) {
+    return false;
+  }
+  const obj = {
+    "[": -1,
+    "]": 1,
+    "(": -2,
+    ")": 2,
+    "{": -3,
+    "}": 3
+  };
 };
