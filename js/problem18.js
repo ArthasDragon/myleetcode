2f9ffa45ef273a67d9b5a6c252ea5a99738f3403
an_ying <ywl123698745>
2018-08-15 17:06:13 +0800

    problem18

--- js/problem18.js
+++ js/problem18.js
@@ -67,4 +67,51 @@ var fourOriginSum = function(nums, target) {
  * @param {number} target
  * @return {number[][]}
  */
-var fourSum = function(nums, target) {};
+var fourSum = function(nums, target) {
+  let result = [];
+  //不是数组或长度小于4返回空
+  if (!Array.isArray(nums) || nums.length < 4) {
+    return result;
+  }
+  //长度等于4 直接判断和是否等于target
+  if (nums.length === 4) {
+    return target === nums[0] + nums[1] + nums[2] + nums[3] ? [nums] : [];
+  }
+  let numsArr = nums.sort((a, b) => a - b);
+  const len = numsArr.length;
+  for (let i = 0; i < len - 3; i++) {
+    for (let j = len - 1; j > i + 2; j--) {
+      const minus = target - numsArr[i] - numsArr[j];
+      let left = i + 1;
+      let right = j - 1;
+      while (left < right) {
+        if (minus === numsArr[left] + numsArr[right]) {
+          let res = [numsArr[i], numsArr[left], numsArr[right], numsArr[j]];
+          if (
+            !result.some(
+              r =>
+                r[0] === res[0] &&
+                r[1] === res[1] &&
+                r[2] === res[2] &&
+                r[3] === res[3]
+            )
+          ) {
+            result.push([
+              numsArr[i],
+              numsArr[left],
+              numsArr[right],
+              numsArr[j]
+            ]);
+          }
+          left++;
+          right--;
+        } else if (minus > numsArr[left] + numsArr[right]) {
+          left++;
+        } else {
+          right--;
+        }
+      }
+    }
+  }
+  return result;
+};
