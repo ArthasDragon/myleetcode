945e758d7084fe22f39f47ace91355b88d98e618
Dragon <1641627793@qq.com>
2018-08-17 12:21:58 +0800

    problem3  Longest Substring Without Repeating Characters

--- js/problem3.js
+++ js/problem3.js
@@ -1,4 +1,5 @@
 /**
+ * Longest Substring Without Repeating Characters
  * @param {string} s
  * @return {number}
  */
@@ -8,8 +9,10 @@ let lengthOfLongestSubstring = function(s) {
   let max_len = arr.length ? 1 : 0;
   let behind = -1;
   arr.map((item, index) => {
+    //behind  记录上段字符串结束位置
     behind =
       obj[item] == undefined ? behind : behind > obj[item] ? behind : obj[item];
+    //记录item最后出现的位置
     obj[item] = index;
     max_len = max_len >= index - behind ? max_len : index - behind;
   });
