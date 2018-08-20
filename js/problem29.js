d4b784d7f81ae23dd25707eec31a5daabde3a870
Dragon <1641627793@qq.com>
2018-08-20 17:21:29 +0800

    problem29    Divide Two Integers

---
+++ js/problem29.js
@@ -0,0 +1,18 @@
+/**
+ * Divide Two Integers
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+const divide = function(dividend, divisor) {
+  let result = dividend / divisor;
+  if (result > 2147483647) {
+    result = 2147483647;
+  }
+  if (result < -2147483648) {
+    result = -2147483648;
+  }
+  return result < 0 ? Math.ceil(result) : Math.floor(result);
+};
+
+export default divide;
