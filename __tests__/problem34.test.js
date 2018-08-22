21191da9a801b73f8529c377a2b747e35b7551a8
Dragon <1641627793@qq.com>
2018-08-22 16:09:11 +0800

    problem35  Search Insert Position

--- __tests__/problem34.test.js
+++
@@ -1,6 +0,0 @@
-import searchRange from "@/problem34";
-
-test("Find First and Last Position of Element in Sorted Array", () => {
-  expect(searchRange([5, 7, 7, 8, 8, 10], 8)).toEqual([3, 4]);
-  expect(searchRange([5, 7, 7, 8, 8, 10], 6)).toEqual([-1, -1]);
-});
---
+++ __tests__/problem35.test.js
@@ -0,0 +1,5 @@
+import searchInsert from "@/problem35";
+
+test("Search Insert Position", () => {
+  expect(searchInsert([1, 3, 5, 6], 5)).toBe(2);
+});
