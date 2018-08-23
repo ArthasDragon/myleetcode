76e9de7601aa34d6e5d09e120231c5093b23da22
Dragon <1641627793@qq.com>
2018-08-23 17:39:01 +0800

    optimize other's code

--- js/problem37.js
+++ js/problem37.js
@@ -5,184 +5,83 @@
  */
 
 //version  1
-let solveSudoku = function(board) {
-  let rows = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
-  let cols = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
-  let boxes = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
+
+var solveSudoku = function(board) {
+  let [rows, cols, triples, visit] = [{}, {}, {}, []];
+  let cloneBoard = {};
+  for (let r = 0; r < 9; r++) {
+    for (let c = 0; c < 9; c++) {
+      let t = [Math.floor(r / 3), Math.floor(c / 3)];
+      rows[r] = rows[r] || new Set();
+      cols[c] = cols[c] || new Set();
+      triples[t] = triples[t] || new Set();
+      if (board[r][c] != ".") {
+        rows[r].add(board[r][c]);
+        cols[c].add(board[r][c]);
+        triples[t].add(board[r][c]);
+      } else {
+        visit.push([r, c]);
+      }
+    }
+  }
+
   let allChoose = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  visit = visit.filter(item => {
+    let i = item[0];
+    let j = item[1];
+    let num = board[i][j];
+    let boxIndex = [Math.floor(i / 3), Math.floor(j / 3)];
+    if (num === ".") {
+      let tempArr = allChoose.filter(
+        item =>
+          !triples[boxIndex].has(item) &&
+          !rows[i].has(item) &&
+          !cols[j].has(item)
+      );
 
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      let num = board[i][j];
-      let boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
-      if (num !== ".") {
-        boxes[boxIndex][num] = rows[i][num] = cols[j][num] = true;
-        boxes[boxIndex]["len"] = (boxes[boxIndex]["len"] || 0) + 1;
+      cloneBoard[[i, j]] = tempArr.length === 1 ? tempArr[0] : tempArr;
 
-        rows[i]["len"] = (rows[i]["len"] || 0) + 1;
-        cols[j]["len"] = (cols[j]["len"] || 0) + 1;
+      if (tempArr.length === 1) {
+        board[i][j] = tempArr[0];
+        triples[boxIndex].add(tempArr[0]);
+        rows[i].add(tempArr[0]);
+        cols[j].add(tempArr[0]);
+        return false;
       }
     }
-  }
+    return true;
+  });
 
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      let num = board[i][j];
-      let boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
-      if (num === ".") {
-        let tempArr = allChoose.filter(
-          item => !boxes[boxIndex][item] && !rows[i][item] && !cols[j][item]
-        );
-        board[i][j] = tempArr.length === 1 ? tempArr[0] : tempArr;
-        if (tempArr.length === 1) {
-          boxes[boxIndex][tempArr[0]] = rows[i][tempArr[0]] = cols[j][
-            tempArr[0]
-          ] = true;
-          boxes[boxIndex]["len"] = (boxes[boxIndex]["len"] || 0) + 1;
-          rows[i]["len"] = (rows[i]["len"] || 0) + 1;
-          cols[j]["len"] = (cols[j]["len"] || 0) + 1;
+  function dfs() {
+    if (visit[0] == undefined) {
+      return true;
+    }
+    let [r, c] = visit[0];
+    let t = [Math.floor(r / 3), Math.floor(c / 3)];
+
+    cloneBoard[[r, c]].forEach(dig => {
+      dig = String(dig);
+      if (!rows[r].has(dig) && !cols[c].has(dig) && !triples[t].has(dig)) {
+        board[r][c] = dig;
+        rows[r].add(dig);
+        cols[c].add(dig);
+        triples[t].add(dig);
+        visit.shift();
+
+        if (dfs()) {
+          return true;
+        } else {
+          board[r][c] = ".";
+          rows[r].delete(dig);
+          cols[c].delete(dig);
+          triples[t].delete(dig);
+          visit.unshift([r, c]);
         }
       }
-    }
-  }
-  while (
-    !(
-      rows.every(row => row.len === 9) &&
-      cols.every(col => col.len === 9) &&
-      boxes.every(box => box.len === 9)
-    )
-  ) {
-    for (let i = 0; i < 9; i++) {
-      for (let j = 0; j < 9; j++) {
-        compareRow(board, i, j, boxes, rows, cols);
-        compareBox(board, i, j, boxes, rows, cols);
-        compareCol(board, i, j, boxes, rows, cols);
-        let num = board[i][j];
-        let boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
-        if (Array.isArray(num)) {
-          let tempArr = num.filter(
-            item => !boxes[boxIndex][item] && !rows[i][item] && !cols[j][item]
-          );
-          board[i][j] = tempArr.length === 1 ? tempArr[0] : tempArr;
-          if (tempArr.length === 1) {
-            boxes[boxIndex][tempArr[0]] = rows[i][tempArr[0]] = cols[j][
-              tempArr[0]
-            ] = true;
-            boxes[boxIndex]["len"] = (boxes[boxIndex]["len"] || 0) + 1;
-            rows[i]["len"] = (rows[i]["len"] || 0) + 1;
-            cols[j]["len"] = (cols[j]["len"] || 0) + 1;
-          }
-        }
-      }
-    }
-  }
-};
-
-let compareRow = function(board, i, j, boxes, rows, cols) {
-  let boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
-  let num = board[i][j];
-  if (Array.isArray(num)) {
-    let tempArr = num.filter(
-      item => !boxes[boxIndex][item] && !rows[i][item] && !cols[j][item]
-    );
-    let tempCountMap = {};
-    tempArr.forEach(item => {
-      tempCountMap[item] = 1;
     });
-    for (let k = 0; k < 9; k++) {
-      if (k !== j) {
-        let curNum = board[i][k];
-        if (Array.isArray(curNum)) {
-          curNum.forEach(item => {
-            if (tempCountMap[item]) {
-              delete tempCountMap[item];
-            }
-          });
-        }
-      }
-    }
-    if (Object.keys(tempCountMap).length === 1) {
-      let resNum = Object.keys(tempCountMap)[0];
-      board[i][j] = resNum;
-      boxes[boxIndex][resNum] = rows[i][resNum] = cols[j][resNum] = true;
-      boxes[boxIndex]["len"] = (boxes[boxIndex]["len"] || 0) + 1;
-      rows[i]["len"] = (rows[i]["len"] || 0) + 1;
-      cols[j]["len"] = (cols[j]["len"] || 0) + 1;
-    }
+    return false;
   }
-};
-
-let compareCol = function(board, i, j, boxes, rows, cols) {
-  let boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
-  let num = board[i][j];
-  if (Array.isArray(num)) {
-    let tempArr = num.filter(
-      item => !boxes[boxIndex][item] && !rows[i][item] && !cols[j][item]
-    );
-    let tempCountMap = {};
-    tempArr.forEach(item => {
-      tempCountMap[item] = 1;
-    });
-    for (let k = 0; k < 9; k++) {
-      if (k !== i) {
-        let curNum = board[k][j];
-        if (Array.isArray(curNum)) {
-          curNum.forEach(item => {
-            if (tempCountMap[item]) {
-              delete tempCountMap[item];
-            }
-          });
-        }
-      }
-    }
-    if (Object.keys(tempCountMap).length === 1) {
-      let resNum = Object.keys(tempCountMap)[0];
-      board[i][j] = resNum;
-      boxes[boxIndex][resNum] = rows[i][resNum] = cols[j][resNum] = true;
-      boxes[boxIndex]["len"] = (boxes[boxIndex]["len"] || 0) + 1;
-      rows[i]["len"] = (rows[i]["len"] || 0) + 1;
-      cols[j]["len"] = (cols[j]["len"] || 0) + 1;
-    }
-  }
-};
-
-let compareBox = function(board, i, j, boxes, rows, cols) {
-  let boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
-  let num = board[i][j];
-  if (Array.isArray(num)) {
-    let tempArr = num.filter(
-      item => !boxes[boxIndex][item] && !rows[i][item] && !cols[j][item]
-    );
-    let tempCountMap = {};
-    tempArr.forEach(item => {
-      tempCountMap[item] = 1;
-    });
-    let startRow = Math.floor(boxIndex / 3) * 3;
-    let startCol = (boxIndex % 3) * 3;
-    for (let k = startRow; k < startRow + 3; k++) {
-      for (let l = startCol; l < startCol + 3; l++) {
-        if (k !== i || l !== j) {
-          let curNum = board[k][l];
-          if (Array.isArray(curNum)) {
-            curNum.forEach(item => {
-              if (tempCountMap[item]) {
-                delete tempCountMap[item];
-              }
-            });
-          }
-        }
-      }
-    }
-
-    if (Object.keys(tempCountMap).length === 1) {
-      let resNum = Object.keys(tempCountMap)[0];
-      board[i][j] = resNum;
-      boxes[boxIndex][resNum] = rows[i][resNum] = cols[j][resNum] = true;
-      boxes[boxIndex]["len"] = (boxes[boxIndex]["len"] || 0) + 1;
-      rows[i]["len"] = (rows[i]["len"] || 0) + 1;
-      cols[j]["len"] = (cols[j]["len"] || 0) + 1;
-    }
-  }
+  dfs();
 };
 
 solveSudoku([
