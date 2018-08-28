8c591c943a92c0a7d1bf244bbd8c5bf488872d8d
Dragon <1641627793@qq.com>
2018-08-28 10:23:31 +0800

    40  Combination Sum II

--- js/problem40.js
+++ js/problem40.js
@@ -47,3 +47,31 @@ let combinationSum2 = function(candidates, target) {
 };
 
 export default combinationSum2;
+
+/**
+ * better solution
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+// var combinationSum2 = function(candidates, target) {
+//   const list = [];
+//   backtrack(candidates.sort((a, b) => a - b), 0, target, [], list);
+//   return list;
+// };
+// function backtrack(candidates, index, target, subset, list) {
+//   if (target === 0) {
+//     list.push(subset.slice());
+//   }
+//   if (target <= 0 || index >= candidates.length || candidates[index] > target) {
+//     return;
+//   }
+//   const value = candidates[index];
+//   subset.push(value);
+//   backtrack(candidates, index + 1, target - value, subset, list);
+//   subset.pop();
+//   while (index < candidates.length && candidates[index] === value) {
+//     index += 1;
+//   }
+//   backtrack(candidates, index, target, subset, list);
+// }
