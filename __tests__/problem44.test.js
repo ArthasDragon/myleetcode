fbd9a7888cfce43fdb2298468a1c80e6f4bdb3c3
Dragon <1641627793@qq.com>
2018-09-04 11:10:45 +0800

    44  Wildcard Matching

--- __tests__/problem44.test.js
+++ __tests__/problem44.test.js
@@ -3,5 +3,8 @@ import isMatch from "@/problem44";
 test("Wildcard Matching", () => {
   expect(isMatch("", "**")).toBeTruthy();
   expect(isMatch("", "")).toBeTruthy();
-  expect(isMatch("aaabb", "*a*b")).toBeTruthy();
+  expect(isMatch("abc", "")).toBeFalsy();
+  expect(isMatch("aab", "c*a*b")).toBeFalsy();
+  expect(isMatch("aaabb", "*a*?b")).toBeTruthy();
+  expect(isMatch("aaabb", "*a*?bc")).toBeFalsy();
 });
--- js/problem44.js
+++ js/problem44.js
@@ -4,14 +4,86 @@
  * @param {string} p
  * @return {boolean}
  */
+
+//my solution like problem 10, use matrix
+const ZERO_OR_CHARS = "*";
+const ONE_ANY_CHAR = "?";
 let isMatch = function(s, p) {
-  if (s.length === 0) {
-    return /\**/.test(p);
-  }
   const sLen = s.length;
   const pLen = p.length;
   let matrix = new Array(sLen + 1);
-  matrix.fill(new Array(pLen + 1));
+  matrix = matrix.fill(null).map(() => {
+    return new Array(pLen + 1).fill(null);
+  });
+
+  matrix[0][0] = true;
+  for (let i = 1; i < pLen + 1; i++) {
+    if (p.charAt(i - 1) == ZERO_OR_CHARS && matrix[0][i - 1]) {
+      matrix[0][i] = true;
+    } else {
+      matrix[0][i] = false;
+    }
+  }
+
+  for (let i = 1; i < sLen + 1; i++) {
+    matrix[i][0] = false;
+  }
+
+  for (let i = 1; i < sLen + 1; i++) {
+    for (let j = 1; j < pLen + 1; j++) {
+      let sIndex = i - 1;
+      let pIndex = j - 1;
+      let pChar = p.charAt(pIndex);
+      let sChar = s.charAt(sIndex);
+      if (pChar === ZERO_OR_CHARS) {
+        matrix[i][j] = false;
+        for (let k = 0; k < i + 1; k++) {
+          if (matrix[k][j - 1]) {
+            matrix[i][j] = true;
+          }
+        }
+      } else if (pChar === ONE_ANY_CHAR || pChar === sChar) {
+        matrix[i][j] = matrix[i - 1][j - 1];
+      } else {
+        matrix[i][j] = false;
+      }
+    }
+  }
+
+  return matrix[sLen][pLen];
 };
 
+// isMatch("", "**");
 export default isMatch;
+
+/**
+ * other's better solution
+ * Wildcard Matching
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+// var isMatch = function(s, p) {
+//     var i = 0, j = 0
+//     var idxmatch = -1, idxstart = -1
+//     while(i < s.length) {
+//         if (j < p.length && (p[j] === '?' || p[j] === s[i])) {
+//           i++
+//           j++
+//         }else if (j < p.length && p[j] === '*') {
+//           idxmatch = i
+//           idxstart = j
+//           j++
+//         }else if (idxstart !== -1) {
+//           j = idxstart + 1
+//           idxmatch++
+//           i = idxmatch
+//         }else{
+//           return false
+//         }
+//     }
+//   while(j< p.length && p[j] == '*'){
+//     j++;
+//   }
+//   return j === p.length;
+// };
