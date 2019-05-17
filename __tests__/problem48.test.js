5b28376915ed6e47dc94d91d4bd7accfad735db2
ArthasDragon <1641627793@qq.com>
2019-05-17 16:56:41 +0800

    problem48

--- __tests__/problem48.test.js
+++ __tests__/problem48.test.js
@@ -1,3 +1,7 @@
 import rotate from "@/problem48";
 
-test("Rotate Image", () => {});
+test("Rotate Image", () => {
+  expect([[7, 4, 1], [8, 5, 2], [9, 6, 3]]).toEqual(
+    expect.arrayContaining(rotate([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+  );
+});
--- js/problem48.js
+++ js/problem48.js
@@ -2,4 +2,83 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-const rotate = function(matrix) {};
+const rotate = function(matrix) {
+  const len = matrix.length;
+  const time = Math.floor(len / 2);
+  let next = null;
+  for (let i = 0; i < time; i++) {
+    let x = i;
+    let y = i;
+    for (let j = 0; j < len - 2 * i - 1; j++) {
+      let temp = matrix[x][y];
+
+      for (let k = 0; k < 4 * (len - 1 - 2 * i); k++) {
+        if (x === i) {
+          if (y === len - 1 - i) {
+            next = matrix[++x][y];
+            matrix[x][y] = temp;
+            temp = next;
+          } else {
+            next = matrix[x][++y];
+            matrix[x][y] = temp;
+            temp = next;
+          }
+        } else if (y === len - 1 - i) {
+          if (x === len - 1 - i) {
+            next = matrix[x][--y];
+            matrix[x][y] = temp;
+            temp = next;
+          } else {
+            next = matrix[++x][y];
+            matrix[x][y] = temp;
+            temp = next;
+          }
+        } else if (x === len - 1 - i) {
+          if (y === i) {
+            next = matrix[--x][y];
+            matrix[x][y] = temp;
+            temp = next;
+          } else {
+            next = matrix[x][--y];
+            matrix[x][y] = temp;
+            temp = next;
+          }
+        } else if (y === i) {
+          if (x === i) {
+            next = matrix[x][++y];
+            matrix[x][y] = temp;
+            temp = next;
+          } else {
+            next = matrix[--x][y];
+            matrix[x][y] = temp;
+            temp = next;
+          }
+        }
+      }
+    }
+  }
+  return matrix;
+};
+export default rotate;
+
+//internet better solution
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+// var rotate = function(matrix) {
+//   for (let i = 0; i < matrix.length / 2; i++) {
+//     let temp = matrix[i];
+//     matrix[i] = matrix[matrix.length - 1 - i];
+//     matrix[matrix.length - 1 - i] = temp;
+//   }
+
+//   for (let i = 0; i < matrix.length; i++) {
+//     for (let j = i + 1; j < matrix.length; j++) {
+//       let temp = matrix[i][j];
+//       matrix[i][j] = matrix[j][i];
+//       matrix[j][i] = temp;
+//     }
+//   }
+//   return matrix;
+// };
