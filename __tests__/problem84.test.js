6ec4a43c7a361179c26fb1738b88633d65395122
ArthasDragon <1641627793@qq.com>
2021-05-27 10:17:55 +0800

    problem84

--- __tests__/problem84.test.js
+++ __tests__/problem84.test.js
@@ -1,3 +1,5 @@
 import largestRectangleArea from "@/problem84";
 
-test("Largest Rectangle in Histogram", () => {});
+test("Largest Rectangle in Histogram", () => {
+  expect(largestRectangleArea([2, 1, 5, 6, 2, 3])).toBe(10);
+});
